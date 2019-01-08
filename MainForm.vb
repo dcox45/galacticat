6565c8646c7ffebdb20f112ff3f5bbc7b84d1436
Dylan Cox <dylanwjc@gmail.com>
2019-01-07 22:21:40 -0500

    halfway through collision detection

--- MainForm.vb
+++ MainForm.vb
@@ -1,18 +1,22 @@
 ﻿Public Class MainForm
     Private Sub MainForm_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
-        If e.KeyCode = Keys.Left Then
-            Max.Speed.X = -Max.StartSpeed.X
-            Max.facingRight = False
+        If Max.state = NORMAL Or Max.state = REVIVE Then
+            Max.state = NORMAL
+            If e.KeyCode = Keys.Left Then
+                Max.Speed.X = -Max.StartSpeed.X
+                Max.facingRight = False
+            End If
+
+            If e.KeyCode = Keys.Right Then
+                Max.Speed.X = Max.StartSpeed.X
+                Max.facingRight = True
+            End If
+
+            If e.KeyCode = Keys.Up And Max.onFloor = True Then
+                Max.Speed.Y = -Max.StartSpeed.Y
+            End If
         End If
 
-        If e.KeyCode = Keys.Right Then
-            Max.Speed.X = Max.StartSpeed.X
-            Max.facingRight = True
-        End If
-
-        If e.KeyCode = Keys.Up And Max.onFloor = True Then
-            Max.Speed.Y = -Max.StartSpeed.Y
-        End If
 
 
     End Sub
@@ -24,18 +28,47 @@
     End Sub
 
     Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles Me.Load
-        Call LoadMax()
+        Dim index As Integer
+
+        Call LoadSprite(Max, "Galacticat.png", 15, 11, 17, 300, 380)
+        Max.Speed.X = 0
+        Max.state = NORMAL
+
+        For index = 0 To NUMBADGUYS
+            If index Mod 2 = 0 Then
+                Call LoadSprite(Badguys(index), "Fangs.png", 10, 5, 17, 107, 40)
+            Else
+                Call LoadSprite(Badguys(index), "Fangs.png", 10, -5, 17, 528, 40)
+            End If
+        Next index
+
         Call LoadBackground()
         Call FloorSet()
         Call DrawScreenSet()
-
-
         Timer1.Start()
     End Sub
 
     Private Sub Timer1_Tick(sender As System.Object, e As System.EventArgs) Handles Timer1.Tick
-        Call MoveMax()
+        Dim index As Integer
+
+        Call MoveSprite(Max)
+
+        TimerCounter += 1
+        NumMovingBadguys = TimerCounter / 50
+        If NumMovingBadguys > NUMBADGUYS Then
+            NumMovingBadguys = NUMBADGUYS
+        End If
+
+        For index = 0 To NumMovingBadguys
+            Call MoveSprite(Badguys(index))
+        Next
+
+
+        Call MoveSprite(Badguys(0))
         Call AnimateMax()
+        Call AnimateBadGuy()
+        Call CheckKillMax()
+        Call ReviveMax()
         Call ScreenDraw()
 
     End Sub
@@ -49,11 +82,16 @@
     End Sub
 
     Private Sub ScreenDraw()
+        Dim index As Integer
         g = picDrawOnScreen.CreateGraphics
         offG = Graphics.FromImage(imageOffScreen)
 
         Call BackgroundDraw()
-        Call MaxDraw()
+        Call DrawSprite(Max)
+        For index = 0 To NumMovingBadguys
+            Call DrawSprite(Badguys(index))
+        Next index
+
 
         g.DrawImage(imageOffScreen, 0, 0)
         g.Dispose()
--- ModSprite.vb
+++ ModSprite.vb
@@ -14,110 +14,206 @@
         Dim CellTop As Integer
         Dim facingRight As Boolean
         Dim onFloor As Boolean
+        Dim radius As Integer
+        Dim state As Integer 'the state of the sprite
 
     End Structure
 
     Public Max As Sprite
-    Public Const gravity As Integer = 1
+    Public Const gravity As Integer = 1.3
+    Public Const NUMBADGUYS As Integer = 6
+    Public Const NORMAL As Integer = 0
+    Public Const DEAD As Integer = 0
+    Public Const REVIVE As Integer = 0
 
-    Public Sub LoadMax()
-        Max.Picture = Image.FromFile(IO.Path.GetDirectoryName(Application.ExecutablePath) & "\pics\galacticat.png")
-        Max.CellCount = 15
-        Max.CellWidth = Max.Picture.Width
-        Max.CellHeight = Max.Picture.Height / Max.CellCount
-        Max.StartPosition.X = 200
-        Max.StartPosition.Y = 200
-        Max.Position.X = Max.StartPosition.X
-        Max.Position.Y = Max.StartPosition.Y
-        Max.Speed.X = 0
-        Max.Speed.Y = 0
-        Max.StartSpeed.X = 5
-        Max.StartSpeed.Y = 17
-        Max.facingRight = True
 
-        Max.mRectangle.Width = Max.CellWidth
-        Max.mRectangle.Height = Max.CellHeight
-        Max.mRectangle.X = Max.Position.X
-        Max.mRectangle.Y = Max.Position.Y
+    Public Badguys(NUMBADGUYS) As Sprite
+
+
+    Public NumMovingBadguys As Integer
+    Public TimerCounter As Integer
+
+    Public Sub LoadSprite(ByRef Guy As Sprite, ByVal picname As String, ByVal numCells As Integer,
+                          ByVal xspeed As Integer, ByVal yspeed As Integer, ByVal xpos As Integer,
+                          ByVal ypos As Integer)
+        Guy.Picture = Image.FromFile(IO.Path.GetDirectoryName(Application.ExecutablePath) &
+                                     "\pics\" + picname)
+        Guy.CellCount = numCells
+        Guy.CellWidth = Guy.Picture.Width
+        Guy.CellHeight = Guy.Picture.Height / Guy.CellCount
+        Guy.StartPosition.X = xpos
+        Guy.StartPosition.Y = ypos
+        Guy.Position.X = Guy.StartPosition.X
+        Guy.Position.Y = Guy.StartPosition.Y
+        Guy.Speed.X = xspeed
+        Guy.Speed.Y = yspeed
+        Guy.StartSpeed.X = xspeed
+        Guy.StartSpeed.Y = yspeed
+        Guy.facingRight = True
+        Guy.state = NORMAL
+
+        If Guy.CellWidth < Guy.CellHeight Then
+            Guy.radius = Guy.CellWidth / 2
+        Else
+            Guy.radius = Guy.CellHeight / 2
+        End If
+
+        Guy.mRectangle.Width = Guy.CellWidth
+        Guy.mRectangle.Height = Guy.CellHeight
+        Guy.mRectangle.X = Guy.Position.X
+        Guy.mRectangle.Y = Guy.Position.Y
     End Sub
 
-    Public Sub MoveMax()
-        Max.Speed.Y = GetVerticalSpeed()
-        Max.Position.X += Max.Speed.X
-        Max.Position.Y += Max.Speed.Y
+    Public Sub MoveSprite(ByRef Guy As Sprite)
+        Guy.Speed.Y = GetVerticalSpeed(Guy)
+        Guy.Position.X += Guy.Speed.X
+        Guy.Position.Y += Guy.Speed.Y
 
-        Max.mRectangle.X = Max.Position.X
-        Max.mRectangle.Y = Max.Position.Y
+        Guy.mRectangle.X = Guy.Position.X
+        Guy.mRectangle.Y = Guy.Position.Y
 
-        If Max.Position.X > (650 + Max.CellWidth) Then
-            Max.Position.X = 0 - Max.CellWidth
-        ElseIf Max.Position.X + Max.CellWidth < 0 Then
-            Max.Position.X = 650
+        If Guy.Position.X > (650 + Guy.CellWidth) Then
+            Guy.Position.X = 0 - Guy.CellWidth
+        ElseIf Guy.Position.X + Guy.CellWidth < 0 Then
+            Guy.Position.X = 650
+        End If
+
+        If Guy.Position.X < -10 And Guy.Position.Y > 360 Then
+            Guy.Position.X = 650 - Guy.CellWidth
+            Guy.Position.Y = 20
+        ElseIf Guy.Position.X > backdrop.Width And Guy.Position.Y > 360 Then
+            Guy.Position.X = 50
+            Guy.Position.Y = 20
         End If
 
     End Sub
 
-    Public Sub MaxDraw()
-        offG.DrawImage(Max.Picture, Max.mRectangle, 0, Max.CellTop, Max.CellWidth,
-                       Max.CellHeight, System.Drawing.GraphicsUnit.Pixel)
+    Public Sub DrawSprite(ByRef Guy As Sprite)
+        offG.DrawImage(Guy.Picture, Guy.mRectangle, 0, Guy.CellTop, Guy.CellWidth,
+                       Guy.CellHeight, System.Drawing.GraphicsUnit.Pixel)
     End Sub
 
     Public Sub AnimateMax()
         Max.CellTop += Max.CellHeight
-        If Max.Speed.X > 0 And Max.onFloor = True Then
-            If Max.CellTop > Max.CellHeight * 2 Then
-                Max.CellTop = 0
+        If Max.state = NORMAL Then
+            If Max.Speed.X > 0 And Max.onFloor = True Then
+                If Max.CellTop > Max.CellHeight * 2 Then
+                    Max.CellTop = 0
+                End If
+            ElseIf Max.Speed.X < 0 And Max.onFloor = True Then
+                If Max.CellTop < Max.CellHeight * 3 Or Max.CellTop > Max.CellHeight * 5 Then
+                    Max.CellTop = Max.CellHeight * 3
+                End If
+            ElseIf Max.Speed.X = 0 And Max.onFloor = True Then
+                If Max.facingRight = True Then
+                    Max.CellTop = Max.CellHeight * 6
+                Else
+                    Max.CellTop = Max.CellHeight * 7
+                End If
+            ElseIf Max.onFloor = False Then
+                If Max.facingRight = True Then
+                    Max.CellTop = Max.CellHeight * 8
+                Else
+                    Max.CellTop = Max.CellHeight * 9
+                End If
             End If
-        ElseIf Max.Speed.X < 0 And Max.onFloor = True Then
-            If Max.CellTop < Max.CellHeight * 3 Or Max.CellTop > Max.CellHeight * 5 Then
-                Max.CellTop = Max.CellHeight * 3
+        ElseIf Max.state = DEAD Then
+            If Max.CellTop < Max.CellHeight * 12 Or Max.CellTop > Max.CellHeight * 13 Then
+                Max.CellTop = Max.CellHeight * 12
             End If
-        ElseIf Max.Speed.X = 0 And Max.onFloor = True Then
-            If Max.facingRight = True Then
-                Max.CellTop = Max.CellHeight * 6
-            Else
-                Max.CellTop = Max.CellHeight * 7
-            End If
-        ElseIf Max.onFloor = False Then
-            If Max.facingRight = True Then
-                Max.CellTop = Max.CellHeight * 8
-            Else
-                Max.CellTop = Max.CellHeight * 9
-            End If
+        ElseIf Max.state = REVIVE Then
+            Max.CellTop = Max.CellHeight * 14
         End If
     End Sub
 
-    Public Function GetVerticalSpeed()
-        Dim nextVStep As Integer
-        nextVStep = Max.Speed.Y + gravity
+    Public Sub AnimateBadGuy()
         Dim index As Integer
-        Max.onFloor = False
-        For index = 0 To 7
-            If Max.Position.X + Max.CellWidth > Floors(index).Left And Max.Position.X <
-                    Floors(index).Right Then
-                If nextVStep > 0 Then
-                    If Max.Position.Y + Max.CellHeight <= Floors(index).Top Then
-                        If Max.Position.Y + Max.CellHeight + nextVStep > Floors(index).Top Then
-                            nextVStep = Floors(index).Top - (Max.Position.Y +
-                                +Max.CellHeight)
-                            Max.onFloor = True
-                        End If
-                    End If
+
+        For index = 0 To NumMovingBadguys
+
+            Badguys(index).CellTop += Badguys(index).CellHeight
+            If Badguys(index).Speed.X >= 0 Then
+                If Badguys(index).CellTop > Badguys(index).CellHeight * 1 Then
+                    Badguys(index).CellTop = 0
                 End If
-
-                If nextVStep <= 0 Then
-                    If Max.Position.Y >= Floors(index).Bottom Then
-                        If Max.Position.Y + nextVStep < Floors(index).Bottom Then
-                            nextVStep = Floors(index).Bottom - Max.Position.Y
-
-                        End If
-                    End If
+            ElseIf Badguys(index).Speed.X < 0 Then
+                If Badguys(index).CellTop < Badguys(index).CellHeight * 2 Or Badguys(index).CellTop >
+                        Badguys(index).CellHeight * 3 Then
+                    Badguys(index).CellTop = Badguys(index).CellHeight * 2
                 End If
             End If
         Next index
+    End Sub
+
+    Public Function GetVerticalSpeed(ByRef Guy As Sprite)
+        Dim nextVStep As Integer
+        nextVStep = Guy.Speed.Y + gravity
+        Dim index As Integer
+        If Guy.state = NORMAL Then
+            Guy.onFloor = False
+            For index = 0 To 7
+                If Guy.Position.X + Guy.CellWidth > Floors(index).Left And Guy.Position.X <
+                    Floors(index).Right Then
+                    If nextVStep > 0 Then
+                        If Guy.Position.Y + Guy.CellHeight <= Floors(index).Top Then
+                            If Guy.Position.Y + Guy.CellHeight + nextVStep > Floors(index).Top Then
+                                nextVStep = Floors(index).Top - (Guy.Position.Y +
+                                +Guy.CellHeight)
+                                Guy.onFloor = True
+                            End If
+                        End If
+                    End If
+
+                    If nextVStep <= 0 Then
+                        If Guy.Position.Y >= Floors(index).Bottom Then
+                            If Guy.Position.Y + nextVStep < Floors(index).Bottom Then
+                                nextVStep = Floors(index).Bottom - Guy.Position.Y
+
+                            End If
+                        End If
+                    End If
+                End If
+            Next index
+        End If
         Return nextVStep
-
-
     End Function
+
+    Public Function Collision(ByVal Guy1 As Sprite, ByVal Guy2 As Sprite)
+        Dim a As Integer
+        Dim b As Integer
+        Dim c As Double
+        a = Guy1.Position.X - Guy2.Position.X
+        b = Guy1.Position.Y - Guy2.Position.Y
+        c = Math.Sqrt(a * a + b * b)
+        If c < Guy1.radius + Guy2.radius Then
+            Return True
+        End If
+        Return False
+    End Function
+
+    Public Sub CheckKillMax()
+        Dim index As Integer
+        For index = 0 To NumMovingBadguys
+            If Collision(Max, Badguys(index)) = True And Max.state = NORMAL And
+            Badguys(index).state = NORMAL Then
+                Max.state = DEAD
+                Max.Speed.X = 0
+                Max.Speed.Y = -20
+            End If
+        Next index
+    End Sub
+
+    Public Sub ReviveMax()
+        If Max.state = DEAD And Max.Position.Y > backdrop.Height Then
+            Max.state = REVIVE
+            Max.Position.X = backdrop.Width / 2 - Max.CellWidth / 2
+            Max.Position.Y = -50
+            Max.Speed.Y = 0
+        End If
+        If Max.state = REVIVE And Max.Position.Y > 50 Then
+            Max.Position.Y = 50
+            Max.Speed.Y = 0
+        End If
+    End Sub
 
 End Module
